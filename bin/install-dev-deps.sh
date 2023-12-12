3eee052ed832c3527beb76b9e3535de50e3f1c8d
Ray Huang (N26120969) <51779943+coding-ray@users.noreply.github.com>
2023-12-12 18:05:36 +0800

    feat: include LibTorch as dependency
    Also, fix wrong preamble

--- bin/install-dev-deps.sh
+++ bin/install-dev-deps.sh
@@ -1,11 +1,11 @@
-#!/bin/sbashh
+#!/bin/bash
 # install Rust
-if ! command -v cargo &> /dev/null ; then
+if ! command -v cargo &> /dev/null; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
 
-# install dependecies to build rust-bert
+# install apt dependecies to build rust-bert
 CHECK_INSTALL_CANDIDATES="pkg-config libssl-dev build-essential"
 INSTALL_CANDIDATES=""
 for pkg in $CHECK_INSTALL_CANDIDATES; do
@@ -15,5 +15,26 @@ for pkg in $CHECK_INSTALL_CANDIDATES; do
 done
 
 if [[ ! -z $INSTALL_CANDIDATES ]]; then
+  echo Installing $INSTALL_CANDIDATES ...
   sudo apt install -y $INSTALL_CANDIDATES
+fi
+
+# install LibTorch to run rust-bert
+DEFAULT_LIBTORCH_PATH="$HOME/.local/share/libtorch"
+echo Installing LibTorch to $DEFAULT_LIBTORCH_PATH ...
+echo This may take 3 to 10 minutes.
+if [ -z "$LIBTORCH" ] || ! [ -d $LIBTORCH ]; then
+  mkdir -p $(sed -e "s/\/$//; s/\/[^/]\+$//" <<< $DEFAULT_LIBTORCH_PATH)
+  cd /tmp
+  wget https://download.pytorch.org/libtorch/cu118/libtorch-cxx11-abi-shared-with-deps-2.0.0%2Bcu118.zip
+  unzip -q libtorch-cxx11-abi-shared-with-deps-2.0.0+cu118.zip
+  mv libtorch $DEFAULT_LIBTORCH_PATH
+  if [ -z "$LIBTORCH" ]; then
+    tee -a ~/.bashrc << EOT
+export LIBTORCH=\"$DEFAULT_LIBTORCH_PATH\"
+export LD_LIBRARY_PATH=\"\${LIBTORCH}/lib:\$LD_LIBRARY_PATH\"
+export PATH=\"\$HOME/.local/bin:\$PATH"
+EOT
+    . ~/.bashrc
+  fi
 fi
